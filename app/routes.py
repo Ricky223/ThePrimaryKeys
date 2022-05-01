b97a810f7262d1c8a154404d815ef3ce965410f6
Ruiqi Zhao <rickyzhao223@gmail.com>
2022-05-01 17:13:10 -0500

    table html changed

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,10 @@ from app import db
 from app.forms import RegistrationForm
 from app.player import Player
 import pymysql
+
 GLOBAL_TEAM = ""
+
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -56,8 +59,8 @@ def enter():
     form = EnterTeamName()
     if form.validate_on_submit():
         print(form.team.data, flush=True)
-        currUser = User.query.filter_by(username = current_user.username).first()
-        team = teamsTable(team_choose = form.team.data, user_id = currUser.id)
+        currUser = User.query.filter_by(username=current_user.username).first()
+        team = teamsTable(team_choose=form.team.data, user_id=currUser.id)
         db.session.add(team)
         db.session.commit()
         session['teamName'] = form.team.data
@@ -76,8 +79,8 @@ def enterYear():
     team = session.get('teamName', None)
     if form.validate_on_submit():
         print(form.year.data, flush=True)
-        currUser = User.query.filter_by(username = current_user.username).first()
-        teamToUpdate = teamsTable.query.filter_by(user_id = currUser.id).order_by(teamsTable.timestamp.desc()).first()
+        currUser = User.query.filter_by(username=current_user.username).first()
+        teamToUpdate = teamsTable.query.filter_by(user_id=currUser.id).order_by(teamsTable.timestamp.desc()).first()
         teamToUpdate.year_choose = form.year.data
         db.session.commit()
         session['year'] = form.year.data
@@ -88,30 +91,31 @@ def enterYear():
 @app.route('/tableplayer')
 @login_required
 def table():
-    con = pymysql.connect(host=csi3335sp2022.mysql["location"], user=csi3335sp2022.mysql["user"], password=csi3335sp2022.mysql["password"],
+    con = pymysql.connect(host=csi3335sp2022.mysql["location"], user=csi3335sp2022.mysql["user"],
+                          password=csi3335sp2022.mysql["password"],
                           database=csi3335sp2022.mysql["db"])
     data = []
     with con:
         cur = con.cursor()
         sql1 = """select distinct teamID from team where name = %s"""
-        cur.execute(sql1,session.get('teamName',None))
+        cur.execute(sql1, session.get('teamName', None))
         teamID = cur.fetchone()
 
         # sql = """select concat(p.nameFirst, ' ', p.nameLast) , a.Gs from appearances a natural join people p where a.teamID = %s and yearID = %s;"""
         sqlgetIDs = """select distinct playerID from appearances where teamID = %s and yearID = %s"""
-        tupleIDs = (teamID,session['year'])
-        cur.execute(sqlgetIDs,tupleIDs)
+        tupleIDs = (teamID, session['year'])
+        cur.execute(sqlgetIDs, tupleIDs)
         resultIDs = cur.fetchall()
 
         sqlGetPlayTime = """select G_p, G_c, G_1b, G_2b, G_3b, G_ss, G_lf, G_cf, G_of, G_dh, G_ph, G_pr from appearances where teamID = %s and yearID = %s and playerID = %s"""
         for row in resultIDs:
             tuplePlayTime = (teamID, session['year'], row)
-            cur.execute(sqlGetPlayTime,tuplePlayTime)
+            cur.execute(sqlGetPlayTime, tuplePlayTime)
             playTimeResults = cur.fetchall()
             for row1 in playTimeResults:
-                data.append(Player(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11]))
-
-
+                data.append(
+                    Player(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10],
+                           row[11]))
 
         # tuple1 = (teamID, session.get('year',None))
         # cur.execute(sql,tuple1)
@@ -120,12 +124,12 @@ def table():
         # for row in results:
         #     data.append(Player(row[0],row[1]))
 
-
-    positions = ['Pitcher', 'Catcher', 'First Base Man', 'Second Base Man', 'Third Base Man', 'Short Stop', 'Left Fielder'
+    positions = ['Pitcher', 'Catcher', 'First Base Man', 'Second Base Man', 'Third Base Man', 'Short Stop',
+                 'Left Fielder'
                  'Center Fielder', 'Right Fielder', 'OutFielder', 'Designated Hitter', 'Pinch Hitter', 'Pinch Runner']
     team = session.get('teamName', None)
     year = session.get('year', None)
-    return render_template('playerTable.html', values=data, team=team, year=year, positions = positions)
+    return render_template('playerTable.html', values=data, team=team, year=year, positions=positions)
 
 
 @app.route('/register', methods=['GET', 'POST'])
